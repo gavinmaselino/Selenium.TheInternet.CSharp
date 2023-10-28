18ba5e6717841dd0e8c2d3400bf68f160511e887
Gavin Maselino <gavinmaselino+1@hotmail.com>
2023-10-28 11:06:36 +0100

    Updates to data table logic

--- Selenium.TheInternet.CSharp/Src/Context/DataTableContext.cs
+++ Selenium.TheInternet.CSharp/Src/Context/DataTableContext.cs
@@ -23,79 +23,45 @@ internal class DataTableContext
         var iterationResult = IterateTableTwo(lastname, email);
         Assert.That(iterationResult, Is.EqualTo(expectedwebsite));
     }
-    
+
     private string? IterateTableOne(string lastname, string due)
     {
         var table = _driver.FindElement(By.Id("table1"));
-        string? websiteTxt = null;
-        
-        IList<IWebElement> trCollection = table.FindElements(By.CssSelector("tbody > tr"));
+
+        var trCollection = table.FindElements(By.CssSelector("tbody > tr"));
 
         foreach (var element in trCollection) 
         {
-            IList<IWebElement> tdCollection = element.FindElements(By.CssSelector("td"));
+            var tdCollection = element.FindElements(By.CssSelector("td"));
 
             var lastNameColTxt = tdCollection[0].Text;
             var dueColTxt = tdCollection[3].Text;
             if (lastNameColTxt != lastname || dueColTxt != due) continue;
             
-            foreach (var ele in tdCollection)
-            {
-                if (ele.Text != "edit delete") continue;
-                var colIndex = tdCollection.IndexOf(ele);
-                websiteTxt = tdCollection[colIndex-1].Text;
-            }
+            var websiteTxt = tdCollection[4].Text;
+            return websiteTxt;
         }
-        return websiteTxt;
+        return null;
     }
 
     private string? IterateTableTwo(string lastname, string emailaddress)
     {
         var table = _driver.FindElement(By.CssSelector("table#table2"));
-        var trCollection = table.FindElements(By.CssSelector("tbody > tr"));
-        
-        string? websiteTxt = null;
-        
-        foreach (var element in trCollection)
-        {
-            var tdCollection = element.FindElements(By.CssSelector("td"));
-            var lastNameIndex = 0;
-            var lastNameIndexFound = false;
-            var emailIndex = 0;
-            var emailIndexFound = false;
-            var websiteIndex = 0;
-            var websiteIndexFound = false;
-            
-            foreach (var td in tdCollection)
-            {
-                var attr = td.GetAttribute("class");
-                switch (attr)
-                {
-                    case "last-name":
-                        lastNameIndex = tdCollection.IndexOf(td);
-                        lastNameIndexFound = true;
-                        break;
-                    case "email":
-                        emailIndex = tdCollection.IndexOf(td);
-                        emailIndexFound = true;
-                        break;
-                    case "web-site":
-                        websiteIndex = tdCollection.IndexOf(td);
-                        websiteIndexFound = true;
-                        break;
-                }
-            }
 
-            if (!lastNameIndexFound || !emailIndexFound || !websiteIndexFound) continue;
-            var lastName = tdCollection[lastNameIndex];
-            var email = tdCollection[emailIndex];
-            var website = tdCollection[websiteIndex];
-                
-            if (lastName.Text == lastname && email.Text == emailaddress)
-            {
-                websiteTxt =  website.Text;
-            }
+        var rows = table.FindElements(By.CssSelector("tr"))
+            .Skip(1) // Skip the header row
+            .ToList();
+
+        foreach (var row in rows)
+        {
+            var lastNameTxt = row.FindElement(By.CssSelector(".last-name")).Text;
+            var emailTxt = row.FindElement(By.CssSelector(".email")).Text;
+
+            if (lastNameTxt != lastname || emailTxt != emailaddress) continue;
+
+            var websiteTxt = row.FindElement(By.CssSelector(".web-site")).Text;
+            return websiteTxt;
         }
-        return websiteTxt;
+        return null;
     }
 }
